120993fd345a746bdafcae8cacf702cb0e4551bf
padma-lang <pamidip@hotmail.com>
2021-05-30 16:48:34 -0500

    modify with sub

--- sum.py
+++ sum.py
@@ -8,5 +8,10 @@ Created on Sun May 30 16:21:21 2021
 def sum(a,b):
     print(f'The sum of the numbers is {a+b}')
     
+def sub(c,d):
+    print(f'The subtract of the numbers is {c-d}')  
+    
+    
 sum(2,3)
 sum(5,9)
+sub(9,3)
